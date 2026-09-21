237e66d5b0f454c16ef20d813b985a2049a1b9b9
Admin <Admin@DESKTOP-6V9KEAB>
2026-09-21 15:45:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿int num = 1;
+string day;
+switch (num)
+{
+    case 1:
+        day = "Monday";
+        break;
+
+    case 2:
+        day = "Tuesday";
+        break;
+
+    case 3:
+        day = "Wednesday";
+        break;
+
+    case 4:
+        day = "Thursday";
+        break;
+
+    case 5:
+        day = "Friday";
+        break;
+
+    case 6:
+        day = "Saturday";
+        break;
+
+    case 7:
+        day = "Sunday";
+        break;
+
+}
+Console.WriteLine(day);
